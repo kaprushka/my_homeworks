be8ca6aed5a15c64a263cf28393e8e2b90d186cc
kaprush_ka <anastasiya.kaprielova@mail.ru>
2016-11-07 21:16:40 +0300

    Learning cycles for, while, enumerate and commands break and continue

--- 4. Python_2/python_2.py
+++ 4. Python_2/python_2.py
@@ -1,8 +1,8 @@
-N = input('Введите число')
+N = input('Введите число: ')
 N = int(N, base=10)
 i = 0
 while i < N:
-    a = input('Введите слово')
+    a = input('Введите слово: ')
     if a == 'программирование':
         break
     print(a)
